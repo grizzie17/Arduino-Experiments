c2b395b0a1df85a107d0da5b171e97faf8ef1fad
John Griswold <john@grizzlyweb.com>
2020-07-18 13:22:32 -0500

    low power experiments

---
+++ lowpower/README.md
@@ -0,0 +1,6 @@
+# Speed Tests #
+
+|   mA | Sketch        |
+| ---: | ------------- |
+| 23.2 | empty.ino     |
+| 18.1 | alloutput.ino |
---
+++ lowpower/alloutput/alloutput.ino
@@ -0,0 +1,14 @@
+
+void
+setup()
+{
+    for ( uint8_t i = 2; i <= 13 + 8; ++i )
+    {
+        pinMode( i, OUTPUT );
+    }
+}
+
+
+void
+loop()
+{}
