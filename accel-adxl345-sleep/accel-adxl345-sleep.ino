d0efeb22f0995ef5ca062035ad494a4d651ceb01
John Griswold <john@grizzlyweb.com>
2020-07-18 13:19:41 -0500

    major rewrite of accel-adxl345-sleep

--- accel-adxl345-sleep/accel-adxl345-sleep.ino
+++ accel-adxl345-sleep/accel-adxl345-sleep.ino
@@ -1,117 +1,61 @@
-//
-// test routines to put ADXL345 and the MCU into
-// low power mode.  Then we need to wakup the system
-// if the ADXL345 generates an Activity interrupt.
-//
+#include <ADXL345_setup.h>  // includes SparkFun ADXL345 Library
+#include <WatchDog.h>
+#include <YogiSleep.h>
 
-#include <Sleep_n0m1.h>
-#include <SparkFun_ADXL345.h>  // SparkFun ADXL345 Library
+const uint8_t k_pinINT = 2;
+const uint8_t k_pinLED = 5;  //LED_BUILTIN;
+const uint8_t k_pinLAY = 4;
+const uint8_t kPinSDA = A4;
+const uint8_t kPinSCL = A5;
 
 #ifdef _MSC_VER
 #    define __asm__ __asm
 #endif
 
-const uint8_t ADXL_M_WATERMARK = ( 1 << ADXL345_INT_WATERMARK_BIT );
-const uint8_t ADXL_M_DATA_READY = ( 1 << ADXL345_INT_DATA_READY_BIT );
-const uint8_t ADXL_M_OVERRUNY = ( 1 << ADXL345_INT_OVERRUNY_BIT );
-const uint8_t ADXL_M_ACTIVITY = ( 1 << ADXL345_INT_ACTIVITY_BIT );
-const uint8_t ADXL_M_INACTIVITY = ( 1 << ADXL345_INT_INACTIVITY_BIT );
-const uint8_t ADXL_M_SINGLE_TAP = ( 1 << ADXL345_INT_SINGLE_TAP_BIT );
-const uint8_t ADXL_M_DOUBLE_TAP = ( 1 << ADXL345_INT_DOUBLE_TAP_BIT );
-const uint8_t ADXL_M_FREE_FALL = ( 1 << ADXL345_INT_FREE_FALL_BIT );
+typedef enum orientation_t
+        : uint8_t
+{
+    OR_UNKNOWN,
+    OR_VERTICAL,
+    OR_HORIZONTAL
+} orientation_t;
 
-const uint8_t k_maskActivity = ADXL_M_ACTIVITY | ADXL_M_SINGLE_TAP
-        | ADXL_M_DOUBLE_TAP | ADXL_M_FREE_FALL;
-
-const uint8_t k_maskInterrupt = k_maskActivity | ADXL_M_INACTIVITY;
+orientation_t g_eOrientation = OR_UNKNOWN;
 
 
-/*********** COMMUNICATION SELECTION ***********/
-/*    Comment Out The One You Are Not Using    */
-// ADXL345 adxl = ADXL345( 10 );  // USE FOR SPI COMMUNICATION, ADXL345(CS_PIN);
-ADXL345 adxl = ADXL345();  // USE FOR I2C COMMUNICATION
-
-Sleep g_tSleep;
-
-/****************** INTERRUPT ******************/
-/*      Uncomment If Attaching Interrupt       */
-// Setup pin 2 to be the interrupt pin (for most Arduino Boards)
-uint8_t k_pinINT = 2;
-uint8_t k_pinLED = 13;
-
-volatile unsigned int g_uCountInterrupt = 0;
-volatile unsigned int g_uCopyInterrupt = 0;
+volatile bool g_bWatchDogInterrupt = false;
 
 unsigned long g_uTimeCurrent = 0;
 unsigned long g_uTimePrevious = 0;
 unsigned long g_uTimeInterrupt = 0;
 
-unsigned long g_nActivity = 0;
+bool g_bActiveLaydown = false;
+bool g_bSleepy = false;
+
+
+YogiSleep g_tSleep;
 
 
 void
-accelIntHandler()
+watchdogIntHandler()
 {
-    ++g_uCountInterrupt;
-}
-
-void
-debugStatus( char* sMessage )
-{
-    bool bSts = adxl.status;
-    byte eCode = adxl.error_code;
-
-    Serial.print( sMessage );
-    if ( ADXL345_ERROR == bSts )
-    {
-        Serial.print( ": error-code=" );
-        Serial.print( eCode, HEX );
-    }
-    else
-    {
-        Serial.print( ": Success" );
-    }
-    Serial.println( "" );
-}
-
-
-// SLEEP //
-
-
-void
-sleepADXL()
-{
-    adxl.setLowPower( true );
+    g_bWatchDogInterrupt = true;
 }
 
 
 void
-sleepProcessor()
-{}
-
-void
-enterSleep()
+watchdogSleepNow()
 {
-    Serial.println( "Entering Sleep Mode" );
+    Serial.println( "watchdog Sleep Now " );
+    delay( 200 );
+    g_bSleepy = true;
+    WatchDog::setPeriod( OVF_8000MS );
+    WatchDog::start();
+    g_tSleep.prepareSleep();
+    g_tSleep.powerDown();
 
-    // don't generate inactivity interrupts during sleep
-    adxl.getInterruptSource();
-    adxl.InactivityINT( 0 );
-    // adxl.setLowPower( true );
-
-    delay( 500 );
-
-    g_tSleep.pwrDownMode();
-    g_tSleep.sleepPinInterrupt( k_pinINT, RISING );
-
-    Serial.println( "Wake Up" );
-    accelIntHandler();
-    // adxl.setLowPower( false );
-    adxl.InactivityINT( 1 );
-
-    pinMode( k_pinINT, INPUT );
-    attachInterrupt(
-            digitalPinToInterrupt( k_pinINT ), accelIntHandler, RISING );
+    // wakeup here
+    WatchDog::stop();
 }
 
 
@@ -131,225 +75,127 @@ setup()
     pinMode( k_pinLED, OUTPUT );
     digitalWrite( k_pinLED, LOW );
 
-    g_uCountInterrupt = 0;
+    pinMode( k_pinLAY, OUTPUT );
+    digitalWrite( k_pinLAY, LOW );
+
+    adxl = ADXL345();
     adxlSetup();
 
-    g_nActivity = 0;
     pinMode( k_pinINT, INPUT );
-    interrupts();
+    g_eOrientation = OR_UNKNOWN;
     attachInterrupt(
-            digitalPinToInterrupt( k_pinINT ), accelIntHandler, RISING );
+            digitalPinToInterrupt( k_pinINT ), adxlIntHandler, CHANGE );
+
+    WatchDog::init( watchdogIntHandler, OVF_4000MS );
 
     g_uTimePrevious = 0;
 
+    g_bActiveLaydown = false;
+
     Serial.println( "setup complete" );
 }
+
+
+bool
+isLayingdown()
+{
+    int x, y, z;
+    adxl.readAccel( &x, &y, &z );
+    return abs( z ) < 100 ? true : false;
+}
+
+
+void
+orientationUnknown()
+{
+    g_eOrientation = isLayingdown() ? OR_HORIZONTAL : OR_VERTICAL;
+}
+
+
+void
+orientationVertical()
+{
+    if ( isLayingdown() )
+    {
+        g_eOrientation = OR_HORIZONTAL;
+    }
+    else
+    {
+        g_uTimeCurrent = millis();
+        uint8_t mInterrupts = adxlGetInterrupts();
+        if ( 0 != mInterrupts )
+        {
+            digitalWrite( k_pinLED, HIGH );
+            adxlDataHandler( mInterrupts );
+            g_uTimeInterrupt = millis();
+        }
+    }
+}
+
+void
+orientationHorizontal()
+{
+    // Check if we are laying-down. If we are not
+    // laying-down then switch modes to OR_VERTICAL.
+    if ( isLayingdown() )
+    {
+        if ( ! g_bActiveLaydown )
+        {
+            g_bActiveLaydown = true;
+
+            digitalWrite( k_pinLED, LOW );
+            digitalWrite( k_pinLAY, HIGH );
+            adxl.setInterruptMask( 0 );
+            watchdogSleepNow();
+        }
+        else
+        {
+            g_bWatchDogInterrupt = false;
+            WatchDog::start();
+        }
+    }
+    else
+    {
+        if ( g_bActiveLaydown )
+        {
+            Serial.println( "watchdog Wakeup" );
+            WatchDog::stop();
+            g_bActiveLaydown = false;
+            g_eOrientation = OR_VERTICAL;
+
+            digitalWrite( k_pinLAY, LOW );
+            adxl.setInterruptMask( k_maskAll );
+        }
+    }
+}
+
 
 /****************** MAIN CODE ******************/
 /*     Accelerometer Readings and Interrupt    */
 void
 loop()
 {
+    switch ( g_eOrientation )
+    {
+    case OR_UNKNOWN:
+        orientationUnknown();
+        break;
+    case OR_VERTICAL:
+        orientationVertical();
+        break;
+    case OR_HORIZONTAL:
+        orientationHorizontal();
+        break;
+    default:
+        break;
+    }
+
+
     g_uTimeCurrent = millis();
-    uint8_t mInterrupts = adxlGetInterrupts();
-
-    if ( 0 != mInterrupts )
+    if ( 1000 * 5 < abs( g_uTimeCurrent - g_uTimePrevious ) )
     {
-        // Serial.print( "interrupt mask = " );
-        // Serial.println( mInterrupts, BIN );
-        adxlDataHandler( mInterrupts );
+        g_uTimePrevious = g_uTimeCurrent;
 
-        if ( 0 != ( mInterrupts & ADXL_M_INACTIVITY ) )
-        {
-            enterSleep();
-        }
-        g_uTimeInterrupt = millis();
-        digitalWrite( k_pinLED, HIGH );
-    }
-    else
-    {
-        if ( 1000 * 3 < abs( g_uTimeCurrent - g_uTimeInterrupt ) )
-        {
-            digitalWrite( k_pinLED, LOW );
-            g_nActivity = 0;
-        }
-    }
-}
-
-/********************* ISR *********************/
-/* Look for Interrupts and Triggered Action    */
-
-void
-adxlSetup()
-{
-    Serial.println( "adxlSetup" );
-
-    adxl.powerOn();  // Power on the ADXL345
-    debugStatus( "adxl.powerOn" );
-
-
-    adxl.setRangeSetting( 2 );
-    debugStatus( "adxl.setRangeSetting(2)" );
-    // Give the range settings
-    // Accepted values are 2g, 4g, 8g or 16g
-    // Higher Values = Wider Measurement Range
-    // Lower Values = Greater Sensitivity
-
-
-    // adxl.setRate( 100 );
-
-    adxl.setInterruptLevelBit( 0 );
-    // set interrupts to High
-
-    // adxl.setSpiBit( 0 );
-    // Configure the device to be in 4 wire SPI mode when set to '0' or 3 wire SPI mode when set to 1
-    // Default: Set to 1
-    // SPI pins on the ATMega328: 11, 12 and 13 as reference in SPI Library
-
-    adxl.setActivityXYZ( 1, 1, 1 );
-    debugStatus( "adxl.setActivityXYZ( 1, 1, 1 )" );
-    // Set to activate movement detection in the axes "adxl.setActivityXYZ(X, Y, Z);" (1 == ON, 0 == OFF)
-    adxl.setActivityThreshold( 20 );
-    debugStatus( "adxl.setActivityThreshold( 16 )" );
-    // 62.5mg per increment   // Set activity   // Inactivity thresholds (0-255)
-
-    adxl.setInactivityXYZ( 1, 1, 1 );
-    debugStatus( "adxl.setInactivityXYZ( 1, 1, 1 )" );
-    // Set to detect inactivity in all the axes "adxl.setInactivityXYZ(X, Y, Z);" (1 == ON, 0 == OFF)
-    adxl.setInactivityThreshold( 16 );
-    debugStatus( "adxl.setInactivityThreshold( 20 )" );
-    // 62.5mg per increment
-    // Set inactivity
-    // Inactivity thresholds (0-255)
-    adxl.setTimeInactivity( 20 );
-    // How many seconds of no activity is inactive?
-
-    adxl.setTapDetectionOnXYZ( 1, 1, 1 );
-    // Detect taps in the directions turned ON "adxl.setTapDetectionOnX(X, Y, Z);" (1 == ON, 0 == OFF)
-
-    // Set values for what is considered a TAP and what is a DOUBLE TAP (0-255)
-    adxl.setTapThreshold( 20 );      // 62.5 mg per increment
-    adxl.setTapDuration( 15 );       // 625 μs per increment
-    adxl.setDoubleTapLatency( 80 );  // 1.25 ms per increment
-    adxl.setDoubleTapWindow( 200 );  // 1.25 ms per increment
-
-    // Set values for what is considered FREE FALL (0-255)
-    adxl.setFreeFallThreshold( 7 );
-    // (5 - 9) recommended - 62.5mg per increment
-    adxl.setFreeFallDuration( 30 );
-    // (20 - 70) recommended - 5ms per increment
-
-    // Setting all interupts to take place on INT2 pin
-    adxl.setImportantInterruptMapping( 2, 2, 2, 2, 2 );
-    // Sets "adxl.setEveryInterruptMapping(single tap, double tap, free fall, activity, inactivity);"
-    // Accepts only 1 or 2 values for pins INT1 and INT2. This chooses the pin on the ADXL345 to use for Interrupts.
-    // Default to INT1 pin.
-
-    // Turn on Interrupts for each mode (1 == ON, 0 == OFF)
-    adxl.InactivityINT( 1 );
-    adxl.ActivityINT( 1 );
-    adxl.FreeFallINT( 1 );
-    adxl.doubleTapINT( 1 );
-    adxl.singleTapINT( 1 );
-
-
-    debugStatus( "adxl setup complete" );
-}
-
-
-uint8_t
-adxlGetInterrupts()
-{
-    unsigned int n = 0;
-    // noInterrupts();
-    n = g_uCountInterrupt;
-    if ( 0 != n )
-    {
-        adxl.InactivityINT( 0 );
-        adxl.ActivityINT( 0 );
-        adxl.FreeFallINT( 0 );
-        adxl.doubleTapINT( 0 );
-        adxl.singleTapINT( 0 );
-
-        // noInterrupts();
-        g_uCountInterrupt = 0;
-        // interrupts();
-
-        // Serial.print( "interrupt count = " );
-        // Serial.println( n );
-        int     x, y, z;
-        uint8_t mInterrupts = 0;
-        while ( 0 < n-- )
-        {
-            mInterrupts |= adxl.getInterruptSource();
-            adxl.readAccel( &x, &y, &z );
-        }
-
-        if ( mInterrupts & k_maskActivity )
-            adxl.InactivityINT( 1 );
-        adxl.ActivityINT( 1 );
-        adxl.FreeFallINT( 1 );
-        adxl.doubleTapINT( 1 );
-        adxl.singleTapINT( 1 );
-
-
-        attachInterrupt(
-                digitalPinToInterrupt( k_pinINT ), accelIntHandler, RISING );
-
-        return mInterrupts & (uint8_t)k_maskInterrupt;
-    }
-    else
-    {
-        // interrupts();
-        return 0;
-    }
-}
-
-
-void
-adxlDataHandler( uint8_t mInterrupts )
-{
-    int x, y, z;
-    adxl.readAccel( &x, &y, &z );
-
-    ++g_nActivity;
-    // Free Fall Detection
-    if ( adxl.triggered( mInterrupts, ADXL345_FREE_FALL ) )
-    {
-        Serial.print( "*** FREE FALL *** " );
-        Serial.println( g_nActivity );
-        //add code here to do when free fall is sensed
-    }
-
-    // Inactivity
-    if ( adxl.triggered( mInterrupts, ADXL345_INACTIVITY ) )
-    {
-        Serial.print( "*** INACTIVITY *** " );
-        Serial.println( g_nActivity );
-        //add code here to do when inactivity is sensed
-    }
-
-    // Activity
-    if ( adxl.triggered( mInterrupts, ADXL345_ACTIVITY ) )
-    {
-        Serial.print( "*** ACTIVITY *** " );
-        Serial.println( g_nActivity );
-        //add code here to do when activity is sensed
-    }
-
-    // Double Tap Detection
-    if ( adxl.triggered( mInterrupts, ADXL345_DOUBLE_TAP ) )
-    {
-        Serial.print( "*** DOUBLE TAP *** " );
-        Serial.println( g_nActivity );
-        //add code here to do when a 2X tap is sensed
-    }
-
-    // Tap Detection
-    if ( adxl.triggered( mInterrupts, ADXL345_SINGLE_TAP ) )
-    {
-        Serial.print( "*** TAP *** " );
-        Serial.println( g_nActivity );
-        //add code here to do when a tap is sensed
+        digitalWrite( k_pinLED, LOW );
     }
 }
